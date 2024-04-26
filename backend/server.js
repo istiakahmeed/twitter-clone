4521bfdbe9c3908d34ba604bec38c6dc9c882111
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-04-26 11:08:37 +0600

    auth controller added

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import dotenv from "dotenv";
 import express from "express";
 import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Server is Ready!");
