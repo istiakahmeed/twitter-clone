1ed4ad352c22417961a999296cae03fb08adb3db
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-04-28 22:46:12 +0600

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-import path from "path";
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
-import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+import postRoutes from "./routes/post.route.js";
+import userRoutes from "./routes/user.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
@@ -35,14 +35,14 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-	connectMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
 });
