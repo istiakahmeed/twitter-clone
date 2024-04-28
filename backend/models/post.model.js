78a2ee28e623c49105c5606a8f0cc3bc5ec564a5
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-04-28 21:48:12 +0600

    backend work done

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,41 +1,41 @@
-import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
 
-const postSchema = new Schema(
-  {
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    text: {
-      type: String,
-    },
-    img: {
-      type: String,
-    },
-    likes: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    comments: [
-      {
-        text: {
-          type: String,
-          required: true,
-        },
-        user: {
-          type: Schema.Types.ObjectId,
-          ref: "User",
-          required: true,
-        },
-      },
-    ],
-  },
-  { timestamps: true }
+const postSchema = new mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		text: {
+			type: String,
+		},
+		img: {
+			type: String,
+		},
+		likes: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		comments: [
+			{
+				text: {
+					type: String,
+					required: true,
+				},
+				user: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "User",
+					required: true,
+				},
+			},
+		],
+	},
+	{ timestamps: true }
 );
 
-const Post = model("Post", postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 export default Post;
